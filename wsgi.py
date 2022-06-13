eb9f93f4944cfa35b7ab70621944c47c7d295b2b
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-13 19:48:09 +0800

    subdomain testing 14-3

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from app.main import app
 
 if __name__ == "__main__":
         #port = int(os.environ.get("PORT", 5000))
-        
+        app.config['SERVER_NAME']="hospitally.online"
         app.run()
 	# app.url_map.default_subdomain = "www"
        
