c968939fad8cec0d2c6580a2ee98639649339cc4
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-14 07:58:33 +0800

    testing lazygit

--- main.py
+++
@@ -1,8 +0,0 @@
-def replace(sentence,target_word,replacement_word):
-    while(target_word in sentence):
-        sentence = sentence.replace(target_word,replacement_word)
-    return sentence
-
-print(replace('The quick brown fox jumps over the lazy dog', 'quick', 'slow'))
-
-print(replace('The quick quick quick quick brown fox jumps over the lazy dog', 'quick', 'slow'))
