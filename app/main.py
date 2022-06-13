31208154cc6818976651cdd235e71fc23bde1a5a
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-13 19:43:19 +0800

    subdomain testing 14

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask import request
 import os
 app = Flask(__name__,subdomain_matching=True)
- 
+app.config['SERVER_NAME']="hospitally.online"
+
 @app.route("/")
 def home_view():
-        return request.url + "13s"
+        return request.url + "14s"
         #return os.environ['MY_SERVER_NAME']
 
 @app.route("/", subdomain="static")
