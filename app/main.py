5c872459b78fa12522d364d65b6b514974dadac6
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-13 20:42:36 +0800

    added in the home page

--- app/main.py
+++ app/main.py
@@ -15,19 +15,22 @@ app.config['MYSQL_DB'] = 'hospitally'
 
 mysql = MySQL(app)
 
-@app.route("/")
-def home_view():
-        return request.url + "15"
-        #return os.environ['MY_SERVER_NAME']
+# @app.route("/")
+# def home_view():
+#     return request.url + "15"
+#     #return os.environ['MY_SERVER_NAME']
+@app.route("/", subdomain="www")
+def home_(hospital_name):
+    return "This is the welcome page"
 
 @app.route("/", subdomain="<hospital_name>")
 def static_index(hospital_name):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute(f'SELECT * FROM hospital_portals WHERE hospital = "{hospital_name}"')
     # Fetch one record and return result
-    hospital = cursor.fetchone()
-    if hospital:
-        return f"Welcome {hospital} hospital"
+    row = cursor.fetchone()
+    if row:
+        return f'Welcome {row["hospital"]} hospital'
     else:
         return "It looks like you're hospital isn't registered with us yet. Sign up now!"
 
