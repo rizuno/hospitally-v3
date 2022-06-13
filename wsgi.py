62be9f10131a61faef3d5b9c27153c441f9b3238
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-13 19:04:02 +0800

    subdomain testing 4

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from app.main import app
  
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
-        app.config['SERVER_NAME']='hospitally.online'
+        app.config['SERVER_NAME']='hospitally-app-v1000001.herokuapp.com:5000'
         app.run(debug=True, port=port)
 	# app.url_map.default_subdomain = "www"
        
