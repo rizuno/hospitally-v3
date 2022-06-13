2a92f4e24df7d575636fb4684ba38032beec691d
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-13 20:33:10 +0800

    dynamic subdomain test

--- app/main.py
+++ app/main.py
@@ -1,16 +1,38 @@
 from flask import Flask
 from flask import request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__,subdomain_matching=True)
 app.config['SERVER_NAME']="hospitally.online"
+
+app.config['MYSQL_HOST'] = '188.166.215.64'
+app.config['MYSQL_USER'] = 'hospitally_app'
+app.config['MYSQL_PASSWORD'] = 'wSbt?gXx+hcV8`.h'
+app.config['MYSQL_DB'] = 'hospitally'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home_view():
         return request.url + "15"
         #return os.environ['MY_SERVER_NAME']
 
-@app.route("/", subdomain="static")
-def static_index():
-    """Flask supports static subdomains
-    This is available at static.your-domain.tld"""
-    return "static.your-domain.tld"
+@app.route("/", subdomain="<hospital_name>")
+def static_index(hospital_name):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(f'SELECT * FROM hospital_portals WHERE hospital = {hospital_name}')
+    # Fetch one record and return result
+    hospital = cursor.fetchone()
+    if hospital:
+        return f"Welcome {hospital} hospital"
+    else:
+        return "It looks like you're hospital isn't registered with us yet. Sign up now!"
+
+    # """Flask supports static subdomains
+    # This is available at static.your-domain.tld"""
+
+    # return "static.your-domain.tld"
+
