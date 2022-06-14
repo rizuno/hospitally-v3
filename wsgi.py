cebec78dd12d319ea092a3b831dfe5e6f2be9e33
shambles1812 <cjvillafuerte02@gmail.com>
2022-06-14 10:11:11 +0800

    css configured

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from app.main import app
-
-if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        
-        app.run(debug_mode = True,port=port)
-	# app.url_map.default_subdomain = "www"
-       
