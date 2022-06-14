ef986f165749d7db9acecd6e0b5d0c9d8f838e15
shambles1812 <cjvillafuerte02@gmail.com>
2022-06-14 10:19:19 +0800

    subdomain configurations

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ def home2():
 # def home_view():
 #     return request.url + "15"
 #     #return os.environ['MY_SERVER_NAME']
-@app.route("/", subdomain="www")
-def home():
-    return render_template("index.html")
+# @app.route("/", subdomain="www")
+# def home():
+#     return render_template("index.html")
 
-@app.route("/", subdomain="<hospital_name>")
-def static_index(hospital_name):
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute(f'SELECT * FROM hospital_portals WHERE hospital = "{hospital_name}"')
-    # Fetch one record and return result
-    row = cursor.fetchone()
-    if row:
-        return f'Welcome {row["hospital"]} hospital'
-    else:
-        return "It looks like your hospital isn't registered with us yet. Sign up now!"
+# @app.route("/", subdomain="<hospital_name>")
+# def static_index(hospital_name):
+#     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#     cursor.execute(f'SELECT * FROM hospital_portals WHERE hospital = "{hospital_name}"')
+#     # Fetch one record and return result
+#     row = cursor.fetchone()
+#     if row:
+#         return f'Welcome {row["hospital"]} hospital'
+#     else:
+#         return "It looks like your hospital isn't registered with us yet. Sign up now!"
 
-    # """Flask supports static subdomains
-    # This is available at static.your-domain.tld"""
+#     # """Flask supports static subdomains
+#     # This is available at static.your-domain.tld"""
 
-    # return "static.your-domain.tld"
+#     # return "static.your-domain.tld"
 
 if __name__ == "__main__":
     app.run(debug=True)
