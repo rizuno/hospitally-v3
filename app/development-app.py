996f3782d6c9edc4890c501f0952f39af5fb00ec
cjvillafuerte5923 <simplegon.yt@gmail.com>
2022-06-14 08:58:01 +0800

    welcom page added

---
+++ app/development-app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '188.166.215.64'
+app.config['MYSQL_USER'] = 'hospitally_app'
+app.config['MYSQL_PASSWORD'] = 'wSbt?gXx+hcV8`.h'
+app.config['MYSQL_DB'] = 'hospitally'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
+
+
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   mode: "jit",
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'primary-white': '#FFFFFF', // bg
+      'secondary-navy': '#545F75',
+      'tertiary-orange': '#FF8C71',
+      'orange-500': '#BF6D59', // border color orange buttons
+      'green-col': '#7BBF7D',
+      'green-col-500': '#66A668', // border color of green buttons
+      'red-col': '#F16969',
+      'red-col-500:': '#BA0505' // border color of red buttons
+    },
+    extend: {
+
+    },
+    container: {
+        center: true
+    }
   },
   plugins: [],
 }
